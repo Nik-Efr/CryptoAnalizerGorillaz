6a6fd22ec9b42764df6fb68b0840e438f3707af5
Nik-Efr <7609060@gmail.com>
2024-10-21 16:52:32 +0300

    ADD TestCommit2

--- src/main/java/com/javarush/efremov/TestClassForGit.java
+++ src/main/java/com/javarush/efremov/TestClassForGit.java
@@ -3,5 +3,7 @@ package com.javarush.efremov;
 public class TestClassForGit {
     public static void main(String[] args) {
         System.out.println("Git!");
+        System.out.println("TestString");
     }
+
 }
