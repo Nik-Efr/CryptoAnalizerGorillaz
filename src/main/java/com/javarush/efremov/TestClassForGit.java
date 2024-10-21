53e8c97c45674d22e54bc1056c72e9080bd09b5d
Nik-Efr <7609060@gmail.com>
2024-10-21 17:08:36 +0300

    ADD TestCommit3 Delete

--- src/main/java/com/javarush/efremov/TestClassForGit.java
+++ src/main/java/com/javarush/efremov/TestClassForGit.java
@@ -2,8 +2,6 @@ package com.javarush.efremov;
 
 public class TestClassForGit {
     public static void main(String[] args) {
-        System.out.println("Git!");
-        System.out.println("TestString");
     }
 
 }
